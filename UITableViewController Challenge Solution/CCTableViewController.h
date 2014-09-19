de4d54792e06d33559dc189fceef375fb90b1e63
Tobkal <Musta@Tobkals-MacBook-Air.local>
2014-09-19 09:12:36 +0200

    solved the challenge

--- UITableViewController Challenge Solution/CCTableViewController.h
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Tobkal on 19/09/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CCTableViewController : UITableViewController
 
 @end
-
